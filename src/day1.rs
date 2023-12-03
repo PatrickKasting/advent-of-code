1dc12578ea90a332ff31f2be346b3992ad717f16
Patrick Kasting <patrick.kasting@gmail.com>
2023-12-03 12:18:16 +0100

    First day solved

---
+++ src/day1.rs
@@ -0,0 +1,141 @@
+use itertools::Itertools;
+use regex::Regex;
+
+type CalibrationValue = u32;
+
+fn calibration_value(
+    digit_patterns: &[(Regex, impl Fn(&str) -> CalibrationValue)],
+    line: &str,
+) -> CalibrationValue {
+    let first_and_last_matches = digit_patterns
+        .iter()
+        .flat_map(|(pattern, value)| {
+            let matches = pattern.find_iter(line).collect_vec();
+            if matches.is_empty() {
+                None
+            } else {
+                let first_and_last_match = [matches[0], matches[matches.len() - 1]];
+                Some(first_and_last_match.map(|m| (m.start(), value(m.as_str()))))
+            }
+        })
+        .collect_vec();
+
+    let first_digit = first_and_last_matches
+        .iter()
+        .map(|first_and_last_match| first_and_last_match[0])
+        .min()
+        .expect("every line should contain at least one digit")
+        .1;
+    let last_digit = first_and_last_matches
+        .iter()
+        .map(|first_and_last_match| first_and_last_match[1])
+        .max()
+        .expect("every line should contain at least one digit")
+        .1;
+
+    first_digit * 10 + last_digit
+}
+
+fn sum_of_calibration_values<'word>(
+    digit_patterns: impl IntoIterator<Item = (&'word str, fn(&str) -> CalibrationValue)>,
+    input: &str,
+) -> CalibrationValue {
+    let digit_patterns = digit_patterns
+        .into_iter()
+        .map(|(word, value)| (Regex::new(word).expect("regex should be valid"), value))
+        .collect_vec();
+    input
+        .lines()
+        .map(|line| calibration_value(&digit_patterns, line))
+        .sum()
+}
+
+fn value_from_digit(digit: &str) -> CalibrationValue {
+    digit.parse().expect("the digit should be numeric")
+}
+
+pub fn first(input: String) -> String {
+    let digit_patterns: [(&str, fn(&str) -> CalibrationValue); 1] = [(r"\d", value_from_digit)];
+    sum_of_calibration_values(digit_patterns, &input).to_string()
+}
+
+fn one(_: &str) -> CalibrationValue {
+    1
+}
+
+fn two(_: &str) -> CalibrationValue {
+    2
+}
+
+fn three(_: &str) -> CalibrationValue {
+    3
+}
+
+fn four(_: &str) -> CalibrationValue {
+    4
+}
+
+fn five(_: &str) -> CalibrationValue {
+    5
+}
+
+fn six(_: &str) -> CalibrationValue {
+    6
+}
+
+fn seven(_: &str) -> CalibrationValue {
+    7
+}
+
+fn eight(_: &str) -> CalibrationValue {
+    8
+}
+
+fn nine(_: &str) -> CalibrationValue {
+    9
+}
+
+pub fn second(input: String) -> String {
+    let digit_patterns: [(&str, fn(&str) -> CalibrationValue); 10] = [
+        (r"\d", value_from_digit),
+        ("one", one),
+        ("two", two),
+        ("three", three),
+        ("four", four),
+        ("five", five),
+        ("six", six),
+        ("seven", seven),
+        ("eight", eight),
+        ("nine", nine),
+    ];
+    sum_of_calibration_values(digit_patterns, &input).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn first_example() {
+        let example = crate::example(1, 0);
+        assert_eq!(first(example), 142.to_string());
+    }
+
+    #[test]
+    fn first_input() {
+        let input = crate::input(1);
+        assert_eq!(first(input), 56042.to_string());
+    }
+
+    #[test]
+    fn second_example() {
+        let example = crate::example(1, 1);
+        assert_eq!(second(example), 281.to_string());
+    }
+
+    #[test]
+    fn second_input() {
+        let input = crate::input(1);
+        assert_eq!(second(input), 55358.to_string());
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+mod day1;
+
+use std::fs;
+
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use strum::EnumString;
+
+type Day = usize;
+
+fn day(str: &str) -> Result<Day> {
+    let day = str.parse()?;
+
+    let (min, max) = (1, SOLUTIONS.len());
+    if day > max {
+        Err(anyhow!("exceeds maximum of {}", max))
+    } else if day < min {
+        Err(anyhow!("subceeds minimum of {}", min))
+    } else {
+        Ok(day)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, EnumString)]
+enum Puzzle {
+    #[strum(ascii_case_insensitive)]
+    First,
+
+    #[strum(ascii_case_insensitive)]
+    Second,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Parser)]
+#[command(about, long_about = None)]
+struct CommandLineArguments {
+    /// Which day?
+    #[clap(value_parser=day)]
+    day: Day,
+
+    /// First or second puzzle?
+    puzzle: Puzzle,
+}
+
+fn input(day: usize) -> String {
+    fs::read_to_string(format!("input/{day}.txt"))
+        .expect(&format!("input for day {day} should exist"))
+}
+
+type Solution = fn(String) -> String;
+
+const SOLUTIONS: [(Solution, Solution); 1] = [(day1::first, day1::second)];
+
+fn main() {
+    let command_line_arguments = CommandLineArguments::parse();
+
+    let input = input(command_line_arguments.day);
+    let solution = match command_line_arguments.puzzle {
+        Puzzle::First => SOLUTIONS[command_line_arguments.day - 1].0,
+        Puzzle::Second => SOLUTIONS[command_line_arguments.day - 1].1,
+    };
+    let answer = solution(input);
+    println!("{}", answer);
+}
+
+fn example(day: usize, example: usize) -> String {
+    fs::read_to_string(format!("examples/{day}/{example}.txt")).expect("example should exist")
+}
