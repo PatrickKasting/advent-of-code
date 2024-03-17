6dcb08a6d75f4a1434206d5e4ebbb17c322fd3bd
Patrick Kasting <patrick.kasting@gmail.com>
2024-03-17 07:28:21 +0100

    Refactored solution attempt to 22nd day

--- src/day22.rs
+++ src/day22.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, ops::Range};
+use std::ops::Range;
 
 use itertools::Itertools;
 
@@ -6,70 +6,69 @@ use crate::utilities::number;
 
 type Coordinate = usize;
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Position {
     z: Coordinate,
     y: Coordinate,
     x: Coordinate,
 }
 
-impl Debug for Position {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{},{},{}", self.x, self.y, self.z)
+impl Position {
+    fn new(x: Coordinate, y: Coordinate, z: Coordinate) -> Self {
+        Position { z, y, x }
     }
 }
 
 impl From<&str> for Position {
     fn from(str: &str) -> Self {
-        let mut coordinates = str.split(',').map(number);
+        let mut coordinates = str.split(',');
         let mut coordinate = || {
-            coordinates
+            let coordinate = coordinates
                 .next()
-                .expect("position should have three coordinates")
+                .expect("position should contain three coordinates");
+            number(coordinate)
         };
-        Position {
-            x: coordinate(),
-            y: coordinate(),
-            z: coordinate(),
-        }
+        Position::new(coordinate(), coordinate(), coordinate())
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Brick {
     lower_end: Position,
     upper_end: Position,
 }
 
-impl Debug for Brick {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}~{:?}", self.lower_end, self.upper_end)
+impl Brick {
+    fn new(one_end: Position, other_end: Position) -> Self {
+        let mut ends = [one_end, other_end];
+        ends.sort();
+        Self {
+            lower_end: ends[0],
+            upper_end: ends[1],
+        }
+    }
+
+    fn cross_section_intersects(self, other: Self) -> bool {
+        let not_intersecting = self.upper_end.y < other.lower_end.y
+            || self.lower_end.y > other.upper_end.y
+            || self.upper_end.x < other.lower_end.x
+            || self.lower_end.x > other.upper_end.x;
+        !not_intersecting
+    }
+
+    fn fall_to(&mut self, z: usize) {
+        let height = self.upper_end.z - self.lower_end.z;
+        self.lower_end.z = z;
+        self.upper_end.z = z + height;
     }
 }
 
 impl From<&str> for Brick {
     fn from(line: &str) -> Self {
-        let mut positions = line.split('~').map(Position::from).collect_vec();
-        positions.sort_unstable();
-        Brick {
-            lower_end: positions[0],
-            upper_end: positions[1],
-        }
-    }
-}
-
-impl Brick {
-    fn fall_to(&mut self, z: Coordinate) {
-        let height = self.upper_end.z - self.lower_end.z;
-        self.lower_end.z = z;
-        self.upper_end.z = z + height;
-    }
-
-    fn cross_section_overlaps(self, other: Brick) -> bool {
-        self.upper_end.x >= other.lower_end.x
-            && self.upper_end.y >= other.lower_end.y
-            && self.lower_end.x <= other.upper_end.x
-            && self.lower_end.y <= other.upper_end.y
+        let (left, right) = line
+            .split_once('~')
+            .expect("every line should contain a tilde");
+        Brick::new(Position::from(left), Position::from(right))
     }
 }
 
@@ -77,113 +76,133 @@ fn bricks(input: &str) -> Vec<Brick> {
     input.lines().map(Brick::from).collect_vec()
 }
 
+fn brick_below_index(sorted_bricks: &[Brick], brick_index: usize) -> Option<usize> {
+    let brick = sorted_bricks[brick_index];
+    for (index, &brick_below_candidate) in sorted_bricks[0..brick_index].iter().enumerate().rev() {
+        if brick_below_candidate.cross_section_intersects(brick) {
+            return Some(index);
+        }
+    }
+    None
+}
+
 fn apply_gravity(sorted_bricks: &mut [Brick]) {
     for brick_index in 0..sorted_bricks.len() {
-        let mut brick = sorted_bricks[brick_index];
-        let mut brick_below_index = brick_index;
-        loop {
-            if brick_below_index == 0 {
-                brick.fall_to(1);
-                break;
-            }
-
-            let brick_below = sorted_bricks[brick_below_index - 1];
-            if brick.cross_section_overlaps(brick_below) {
-                brick.fall_to(brick_below.upper_end.z + 1);
-                break;
-            }
-            brick_below_index -= 1;
-        }
-        sorted_bricks[brick_index] = brick;
-        sorted_bricks[brick_below_index..=brick_index].sort_unstable();
+        let (new_brick_index_lower_bound, settled_height) =
+            if let Some(brick_below_index) = brick_below_index(sorted_bricks, brick_index) {
+                let settled_height = sorted_bricks[brick_below_index].upper_end.z + 1;
+                (brick_below_index + 1, settled_height)
+            } else {
+                (0, 1)
+            };
+        sorted_bricks[brick_index].fall_to(settled_height);
+        sorted_bricks[new_brick_index_lower_bound..=brick_index].sort_unstable();
     }
 }
 
-fn settled_bricks(input: &str) -> Vec<Brick> {
-    let mut bricks = bricks(input);
-    bricks.sort_unstable();
-    apply_gravity(&mut bricks);
-    bricks
+fn bricks_at_height_indices(bricks: &[Brick], z: Coordinate) -> Range<usize> {
+    let zero_this_height = Position::new(0, 0, z);
+    let lower_bound = Brick::new(zero_this_height, zero_this_height);
+    let zero_next_height = Position::new(0, 0, z + 1);
+    let upper_bound = Brick::new(zero_next_height, zero_next_height);
+    let lower_bound = match bricks.binary_search(&lower_bound) {
+        Ok(lower_bound) | Err(lower_bound) => lower_bound,
+    };
+    let upper_bound = match bricks.binary_search(&upper_bound) {
+        Ok(upper_bound) | Err(upper_bound) => upper_bound,
+    };
+    lower_bound..upper_bound
 }
 
-fn indices_of_bricks_at_height(sorted_bricks: &[Brick], z: Coordinate) -> Range<usize> {
-    let singleton = |position| Brick {
-        lower_end: position,
-        upper_end: position,
-    };
-    let binary_search = |brick| match sorted_bricks.binary_search(&brick) {
-        Ok(index) | Err(index) => index,
-    };
-    let start = binary_search(singleton(Position {
-        x: Coordinate::MIN,
-        y: Coordinate::MIN,
-        z,
-    }));
-    let end = binary_search(singleton(Position {
-        x: Coordinate::MIN,
-        y: Coordinate::MIN,
-        z: z + 1,
-    }));
-    start..end
-}
-
-fn indices_of_supported_bricks(sorted_bricks: &[Brick], brick_index: usize) -> Vec<usize> {
-    let brick = sorted_bricks[brick_index];
-    indices_of_bricks_at_height(sorted_bricks, brick.upper_end.z + 1)
-        .filter(|&brick_above_index| brick.cross_section_overlaps(sorted_bricks[brick_above_index]))
+fn supported_brick_indices(settled_bricks: &[Brick], brick: Brick) -> Vec<usize> {
+    let supported_bricks_height = brick.upper_end.z + 1;
+    bricks_at_height_indices(settled_bricks, supported_bricks_height)
+        .filter(|&above_brick_index| {
+            settled_bricks[above_brick_index].cross_section_intersects(brick)
+        })
         .collect_vec()
 }
 
-fn indices_of_disintegrable_bricks(sorted_bricks: &[Brick]) -> impl Iterator<Item = usize> {
-    let supported_bricks = (0..sorted_bricks.len())
-        .map(|brick| indices_of_supported_bricks(sorted_bricks, brick))
+fn all_supported_brick_indices(settled_bricks: &[Brick]) -> Vec<Vec<usize>> {
+    settled_bricks
+        .iter()
+        .map(|&brick| supported_brick_indices(settled_bricks, brick))
+        .collect_vec()
+}
+
+fn all_supporting_brick_indices(all_supported_brick_indices: &[Vec<usize>]) -> Vec<Vec<usize>> {
+    let mut all_supporting_brick_indices = all_supported_brick_indices
+        .iter()
+        .map(|_| vec![])
         .collect_vec();
-    let mut supporting_bricks = vec![vec![]; sorted_bricks.len()];
-    for (supporting, supported) in supported_bricks.iter().enumerate() {
-        for &supported in supported {
-            supporting_bricks[supported].push(supporting);
+    for (below_brick_index, above_brick_indices) in all_supported_brick_indices.iter().enumerate() {
+        for &above_brick_index in above_brick_indices {
+            all_supporting_brick_indices[above_brick_index].push(below_brick_index);
         }
     }
+    all_supporting_brick_indices
+}
 
-    supported_bricks
-        .into_iter()
-        .enumerate()
-        .filter(move |(_, supported)| {
-            supported
-                .iter()
-                .all(|&supported| supporting_bricks[supported].len() > 1)
-        })
-        .map(|(brick, _)| brick)
+fn disintegrable(
+    all_supported_brick_indices: &[Vec<usize>],
+    all_supporting_brick_indices: &[Vec<usize>],
+    brick_index: usize,
+) -> bool {
+    all_supported_brick_indices[brick_index]
+        .iter()
+        .all(|&supported_brick_index| all_supporting_brick_indices[supported_brick_index].len() > 1)
+}
+
+fn disintegrable_brick_indices(settled_bricks: &[Brick]) -> impl Iterator<Item = usize> {
+    let all_supported_brick_indices = all_supported_brick_indices(settled_bricks);
+    let all_supporting_brick_indices = all_supporting_brick_indices(&all_supported_brick_indices);
+    (0..settled_bricks.len()).filter(move |&brick_index| {
+        disintegrable(
+            &all_supported_brick_indices,
+            &all_supporting_brick_indices,
+            brick_index,
+        )
+    })
 }
 
 pub fn first(input: &str) -> String {
-    let bricks = settled_bricks(input);
-    indices_of_disintegrable_bricks(&bricks).count().to_string()
+    let mut bricks = bricks(input);
+    bricks.sort_unstable();
+    apply_gravity(&mut bricks);
+    disintegrable_brick_indices(&bricks).count().to_string()
 }
 
 pub fn second(_input: &str) -> String {
-    todo!()
+    todo!();
 }
 
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
 
-    use crate::{input, tests::*, Input};
+    use crate::{input, tests::*, Input, Puzzle};
 
     use super::*;
 
     const DAY: usize = 22;
 
+    fn settled_bricks_from_input(input: Input) -> Vec<Brick> {
+        let mut bricks = bricks(&crate::input(DAY, input));
+        bricks.sort_unstable();
+        apply_gravity(&mut bricks);
+        bricks
+    }
+
     #[test]
-    fn overlapping_cross_sections() {
+    fn cross_section_intersects() {
         let bricks = bricks(&input(DAY, Input::Example(0)));
         let bricks_with_indices = bricks.iter().enumerate();
+
         let actual: HashSet<[usize; 2]> = bricks_with_indices
             .clone()
             .cartesian_product(bricks_with_indices)
-            .filter(|((_, left), (_, right))| left.cross_section_overlaps(**right))
-            .map(|((left, _), (right, _))| [left, right])
+            .filter(|((_, left), (_, right))| left.cross_section_intersects(**right))
+            .map(|((left_index, _), (right_index, _))| [left_index, right_index])
             .collect();
 
         let expected = [
@@ -210,8 +229,8 @@ mod tests {
     }
 
     #[test]
-    fn settled_stack() {
-        let actual = settled_bricks(&input(DAY, Input::Example(0)));
+    fn settled_bricks() {
+        let bricks = settled_bricks_from_input(Input::Example(0));
         let expected = "\
             1,0,1~1,2,1\n\
             0,0,2~2,0,2\n\
@@ -221,14 +240,22 @@ mod tests {
             0,1,4~2,1,4\n\
             1,1,5~1,1,6\n\
         ";
-        assert_eq!(actual, bricks(expected));
+        assert_eq!(bricks, super::bricks(expected));
     }
 
     #[test]
-    fn supported_bricks() {
-        let settled_bricks = settled_bricks(&input(DAY, Input::Example(0)));
-        let function = |index| indices_of_supported_bricks(&settled_bricks, index);
-        let cases = 0..settled_bricks.len();
+    fn bricks_at_height_indices() {
+        let bricks = settled_bricks_from_input(Input::Example(0));
+        test_cases(
+            |z| super::bricks_at_height_indices(&bricks, z),
+            [2, 3, 4],
+            [1..3, 3..5, 5..6],
+        );
+    }
+
+    #[test]
+    fn supported_brick_indices() {
+        let bricks = settled_bricks_from_input(Input::Example(0));
         let expected = [
             vec![1, 2],
             vec![3, 4],
@@ -238,18 +265,23 @@ mod tests {
             vec![6],
             vec![],
         ];
-        test_cases(function, cases, expected);
+        test_cases(
+            |&brick| super::supported_brick_indices(&bricks, brick),
+            bricks.iter(),
+            expected,
+        );
     }
 
     #[test]
-    fn disintegrable_bricks() {
-        let settled_bricks = settled_bricks(&input(DAY, Input::Example(0)));
-        let actual: HashSet<usize> = indices_of_disintegrable_bricks(&settled_bricks).collect();
-        assert_eq!(actual, HashSet::from([1, 2, 3, 4, 6]));
+    fn disintegrable_brick_indices() {
+        let bricks = settled_bricks_from_input(Input::Example(0));
+        let actual = super::disintegrable_brick_indices(&bricks).collect_vec();
+        let expected = vec![1, 2, 3, 4, 6];
+        assert_eq!(actual, expected);
     }
 
-    // #[test]
-    // fn first_example() {
-    //     test_on_input(DAY, Puzzle::First, Input::Example(0), 5);
-    // }
+    #[test]
+    fn first_example() {
+        test_on_input(DAY, Puzzle::First, Input::Example(0), 5);
+    }
 }
