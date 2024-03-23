2d8580f786a449163344b17024b5287205871aa4
Patrick Kasting <patrick.kasting@gmail.com>
2024-03-23 17:37:20 +0100

    Added correct tests to all solutions of 2022

--- src/advent2022/day08.rs
+++ src/advent2022/day08.rs
@@ -8,20 +8,28 @@ pub fn second(_input: &str) -> String {
 
 #[cfg(test)]
 mod tests {
+    // use super::super::tests::test_on_input;
+    // use crate::{Input, Puzzle};
 
-    #[test]
-    fn first_example() {
-    }
+    // const DAY: usize = 8;
 
-    #[test]
-    fn first_input() {
-    }
+    // #[test]
+    // fn first_example() {
+    //     test_on_input(DAY, Puzzle::First, Input::Example(0), 95437);
+    // }
 
-    #[test]
-    fn second_example() {
-    }
+    // #[test]
+    // fn first_input() {
+    //     test_on_input(DAY, Puzzle::First, Input::PuzzleInput, 1_444_896);
+    // }
 
-    #[test]
-    fn second_input() {
-    }
+    // #[test]
+    // fn second_example() {
+    //     test_on_input(DAY, Puzzle::Second, Input::Example(0), 24_933_642);
+    // }
+
+    // #[test]
+    // fn second_input() {
+    //     test_on_input(DAY, Puzzle::Second, Input::PuzzleInput, 404_395);
+    // }
 }
