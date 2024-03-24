c5bd877e75895b04c6093cde5baa330fbb3fc12b
Patrick Kasting <patrick.kasting@gmail.com>
2024-03-24 14:07:56 +0100

    Refactored day 1 of 2023

--- src/advent2023/day01.rs
+++ src/advent2023/day01.rs
@@ -4,6 +4,54 @@ use itertools::Itertools;
 
 type CalibrationValue = usize;
 
+pub fn first(input: &str) -> String {
+    sum_of_calibration_values(input, false).to_string()
+}
+
+pub fn second(input: &str) -> String {
+    sum_of_calibration_values(input, true).to_string()
+}
+
+fn sum_of_calibration_values(input: &str, consider_numerals: bool) -> CalibrationValue {
+    input
+        .lines()
+        .map(|line| calibration_value(line, consider_numerals))
+        .sum()
+}
+
+fn calibration_value(line: &str, consider_numerals: bool) -> CalibrationValue {
+    let [mut first_value, mut last_value] = first_and_last_digit(line);
+    if consider_numerals {
+        if let Some([first_numeral, last_numeral]) = first_and_last_numeral(line) {
+            first_value = cmp::min(first_value, first_numeral);
+            last_value = cmp::max(last_value, last_numeral);
+        }
+    }
+    first_value.1 * 10 + last_value.1
+}
+
+fn first_and_last_digit(line: &str) -> [(usize, CalibrationValue); 2] {
+    [str::find, str::rfind].map(|finder| {
+        let index = finder(line, char::is_numeric).expect("line should contain a digit");
+        (index, (line.as_bytes()[index] - b'0') as CalibrationValue)
+    })
+}
+
+fn first_and_last_numeral(line: &str) -> Option<[(usize, CalibrationValue); 2]> {
+    let numerals = NUMERALS
+        .into_iter()
+        .flat_map(|(numeral, value)| {
+            line.match_indices(numeral)
+                .map(move |(index, _)| (index, value))
+        })
+        .collect_vec();
+    (!numerals.is_empty()).then(|| {
+        [Iterator::min, Iterator::max].map(|extremum| {
+            extremum(numerals.iter().copied()).expect("'numerals' should not be empty")
+        })
+    })
+}
+
 const NUMERALS: [(&str, CalibrationValue); 9] = [
     ("one", 1),
     ("two", 2),
@@ -15,51 +63,6 @@ const NUMERALS: [(&str, CalibrationValue); 9] = [
     ("eight", 8),
     ("nine", 9),
 ];
-
-fn first_and_last_digit(line: &str) -> [Option<(usize, CalibrationValue)>; 2] {
-    [str::find, str::rfind].map(|finder| {
-        let index = finder(line, char::is_numeric);
-        index.map(|index| (index, (line.as_bytes()[index] - b'0') as CalibrationValue))
-    })
-}
-
-fn first_and_last_numeral(line: &str) -> [Option<(usize, CalibrationValue)>; 2] {
-    let numerals = NUMERALS
-        .into_iter()
-        .flat_map(|(numeral, value)| {
-            line.match_indices(numeral)
-                .map(move |(index, _)| (index, value))
-        })
-        .collect_vec();
-    [Iterator::min, Iterator::max].map(|extremum| extremum(numerals.iter().copied()))
-}
-
-fn calibration_value(consider_numerals: bool, line: &str) -> CalibrationValue {
-    let [mut first_value, mut last_value] = first_and_last_digit(line);
-    if consider_numerals {
-        let [first_numeral, last_numeral] = first_and_last_numeral(line);
-        first_value = cmp::min(first_value.or(first_numeral), first_numeral.or(first_value));
-        last_value = cmp::max(last_value.or(last_numeral), last_numeral.or(last_value));
-    }
-    let [first_value, last_value] = [first_value, last_value]
-        .map(|value| value.expect("line should contain at least one digit").1);
-    first_value * 10 + last_value
-}
-
-fn sum_of_calibration_values(consider_numerals: bool, input: &str) -> CalibrationValue {
-    input
-        .lines()
-        .map(|line| calibration_value(consider_numerals, line))
-        .sum()
-}
-
-pub fn first(input: &str) -> String {
-    sum_of_calibration_values(false, input).to_string()
-}
-
-pub fn second(input: &str) -> String {
-    sum_of_calibration_values(true, input).to_string()
-}
 
 #[cfg(test)]
 mod tests {
@@ -80,7 +83,7 @@ mod tests {
 
     #[test]
     fn second_example() {
-        test_on_input(DAY, Puzzle::Second, Input::Example(1), 281);
+        test_on_input(DAY, Puzzle::Second, Input::Example(1), 281 - 83);
     }
 
     #[test]
