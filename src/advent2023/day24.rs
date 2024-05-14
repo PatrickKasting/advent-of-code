b4ba6be81972a7536d590729abfd56c79d8ffff8
Patrick Kasting <patrick.kasting@gmail.com>
2024-05-14 17:25:48 +0200

    Added documentation of the math in second part of 24th day of 2023

--- src/advent2023/day24.rs
+++ src/advent2023/day24.rs
@@ -48,37 +48,53 @@ fn path_intersection((s0, v0): Hailstone, (s1, v1): Hailstone) -> Option<[Coordi
     }
 }
 
-fn is_inside_area(area: RangeInclusive<Coordinate>, point: [Coordinate; 2]) -> bool {
+fn is_inside_area(range: RangeInclusive<Coordinate>, point: [Coordinate; 2]) -> bool {
     point
         .into_iter()
-        .all(|coordinate| area.contains(&coordinate))
+        .all(|coordinate| range.contains(&coordinate))
 }
 
-/// Let `s0` and `v0` be the initial position and velocity of the rock. These are the
-/// values, we want to determine. We do this by setting up a system of linear equations and solving
-/// using Gaussian elimination.
+/// Returns the initial position and velocity needed for the rock to hit every hailstone.
 ///
-/// Let `s[i]` and `v[i]` be the initial position and velocity of the `i`'th hailstone. Let
-/// `t[i]` denote the time of collision between the rock and the `i`'th hailstone. For every `i`, we
-/// must have `s0 + v0 * t[i] = s[i] + v[i] * t[i]`. Rearranging gives
-/// `s0 + -s[i] + (v0 + -v[i]) * t[i] = 0`. Since `t[i]` is scalar, `s0 + -s[i]` and `v0 + -v[i]`
-/// must be parallel for the vector sum to equal `0`. Thus, `(s0 + -s[i]) x (v0 + -v[i]) = 0`.
+/// # Correctness
 ///
-/// Distributivity of the cross product yields `((s0 + -s[i]) x v0) + ((s0 + -s[i]) x -v[i]) = 0`.
-/// Anticommutativity of the cross product gives `(v0 x (s0 + -s[i])) + (-v[i] x (s0 + -s[i])) = 0`.
-/// Invoking distributivity once again results in
-/// `(v0 x s0) + (v0 x -s[i]) + (-v[i] x s0) + (-v[i] x -s[i]) = 0`.
+/// The overall strategy is to set up system of linear equations that we can solve using our solver.
 ///
-/// Note that the term `v0 x s0` is not linear in the unknowns, so it is seemingly preventing us
-/// from having a system of linear equations. However, `v0 x s0` does not depend on `i`, so this
-/// term can be equated for any pair of `i`s.
+/// Let `s0` and `v0` be the initial position and velocity of the rock. Let `s[i]` and `v[i]` be the
+/// initial position and velocity of the `i`'th hailstone. Let `t[i]` denote the time of collision
+/// between the rock and the `i`'th hailstone. The unknowns are `s0`, `v0`, and the `t[i]`s.
 ///
-/// Let us equate the above equation for `i = 1` and `i = 2`:
-/// `(v0 x -s[1]) + (-v[1] x s0) + (-v[1] x -s[1]) = (v0 x -s[2]) + (-v[2] x s0) + (-v[2] x -s[2])`.
-/// Coordinate by coordinate:
-/// `(v0[y] * -s[1][z] - v0[z] * -s[1][y]) + (-v[1][y] * s0[z] - -v[1][z] * s0[y])`
-/// `(v0[z] * -s[1][x] - v0[x] * -s[1][z]) + (-v[1][z] * s0[x] - -v[1][x] * s0[z])`
-/// `(v0[x] * -s[1][y] - v0[y] * -s[1][x]) + (-v[1][x] * s0[y] - -v[1][y] * s0[x])`
+/// For every `i`, we have a collision: `s0 + v0 * t[i] == s[i] + v[i] * t[i]`. The term `v0 * t[i]`
+/// is not linear in the unknowns, so this equation does not fit in our system of linear equations.
+/// Rearranging gives `s[i] + -s0 + (v[i] + -v0) * t[i] == 0` and since `t[i]` is scalar,
+/// `s[i] + -s0` and `v[i] + -v0` must be parallel for the vector sum to equal `0`. Thus,
+/// `(s[i] + -s0) x (v[i] + -v0) == 0`.
+///
+/// Distributivity of the cross product yields `((s[i] + -s0) x v[i]) + ((s[i] + -s0) x -v0) == 0`.
+/// Anticommutativity of the cross product gives
+/// `-(v[i] x (s[i] + -s0)) + -(-v0 x (s[i] + -s0)) == 0`. Multiplying both sides by `-1` gives
+/// `(v[i] x (s[i] + -s0)) + (-v0 x (s[i] + -s0)) == 0`. Invoking distributivity once again results
+/// in `(v[i] x s[i]) + (v[i] x -s0) + (-v0 x s[i]) + (-v0 x -s0) == 0`.
+///
+/// Note that the term `-v0 x -s0` is not linear in the unknowns, so it is seemingly preventing us
+/// from having a system of linear equations. However, `-v0 x -s0` does not depend on `i`, so this
+/// term can be equated for any pair of `i`s, leaving us with linear equations! The other three
+/// terms `(v[i] x s[i]) + (v[i] x -s0) + (-v0 x s[i])` can be written as a product of the following
+/// matrix `A[i]` and the vector `v == [s0[x], s0[y], s0[z], v0[x], v0[y], v0[z], 1]`:
+///
+/// ```ignore
+/// |        0,  v[i][z], -v[i][y],        0, -s[i][z],  s[i][y], v[i][y]*s[i][z]-v[i][z]*s[i][y] |
+/// | -v[i][z],        0,  v[i][x],  s[i][z],        0, -s[i][x], v[i][z]*s[i][x]-v[i][x]*s[i][z] |
+/// |  v[i][y], -v[i][x],        0, -s[i][y],  s[i][x],        0, v[i][x]*s[i][y]-v[i][y]*s[i][x] |
+/// ```
+///
+/// Picking the pairs of `i`s `(0, 1)` and `(0, 2)` yields `A[0] . v == A[1] . v` and
+/// `A[0] . v == A[2] . v`, which are six linear equations. This is enough to determine our six
+/// unknowns `s0[x]`, `s0[y]`, `s0[z]`, `v0[x]`, `v0[y]`, and `v0[z]`.
+///
+/// To bring these equations into standard matrix-equation form, we subtract the right-hand sides,
+/// which gives `(A[0] - A[1]) . v == 0` and `(A[0] - A[2]) . v == 0`. Finally, moving the constants
+/// to the now-empty right-hand sides yields a system of linear equations that can be solved.
 fn initial_position_and_velocity(hailstones: &[Hailstone]) -> (Position, Velocity) {
     let mut augmented_matrix: [[Coordinate; 7]; 6] = Default::default();
     augmented_matrix[0..3].copy_from_slice(&equations(hailstones[0], hailstones[1]));
@@ -92,6 +108,16 @@ fn initial_position_and_velocity(hailstones: &[Hailstone]) -> (Position, Velocit
     }
 }
 
+/// Let `s[i]` and `v[i]` be the initial position and velocity of the `i`'th hailstone. Then, let
+/// `A[i]` be the following matrix (notice the signs of the last column):
+///
+/// ```ignore
+/// |        0,  v[i][z], -v[i][y],        0, -s[i][z],  s[i][y], -v[i][y]*s[i][z]+v[i][z]*s[i][y] |
+/// | -v[i][z],        0,  v[i][x],  s[i][z],        0, -s[i][x], -v[i][z]*s[i][x]+v[i][x]*s[i][z] |
+/// |  v[i][y], -v[i][x],        0, -s[i][y],  s[i][x],        0, -v[i][x]*s[i][y]+v[i][y]*s[i][x] |
+/// ```
+///
+/// Given the hailstones `j` and `k`, this function returns `A[j] - A[k]`.
 fn equations(left: Hailstone, right: Hailstone) -> [[Coordinate; 7]; 3] {
     let [lhs, rhs] = [coefficients(left), coefficients(right)];
     let mut equations: [[Coordinate; 7]; 3] = lhs
@@ -107,6 +133,13 @@ fn equations(left: Hailstone, right: Hailstone) -> [[Coordinate; 7]; 3] {
     equations
 }
 
+/// Given the `i`th hailstone `(s[i], v[i])`, return the following matrix:
+///
+/// ```ignore
+/// |        0,  v[i][z], -v[i][y],        0, -s[i][z],  s[i][y], v[i][y]*s[i][z]-v[i][z]*s[i][y] |
+/// | -v[i][z],        0,  v[i][x],  s[i][z],        0, -s[i][x], v[i][z]*s[i][x]-v[i][x]*s[i][z] |
+/// |  v[i][y], -v[i][x],        0, -s[i][y],  s[i][x],        0, v[i][x]*s[i][y]-v[i][y]*s[i][x] |
+/// ```
 fn coefficients(([sx, sy, sz], [vx, vy, vz]): Hailstone) -> [[Coordinate; 7]; 3] {
     [
         [0.0, vz, -vy, 0.0, -sz, sy, vy * sz - vz * sy],
