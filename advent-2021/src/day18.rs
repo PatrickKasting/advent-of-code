d2b8c5e2c14e0f742eb782a2a5508f7ec776dd0b
Patrick Kasting <patrick.kasting@gmail.com>
2024-09-08 13:45:31 +0200

    Day 18 of 2021 solved again

--- advent-2021/src/day18.rs
+++ advent-2021/src/day18.rs
@@ -1,185 +1,200 @@
-use std::{cmp::max, fmt::Display};
+use std::fmt::Display;
 
-type Number = usize;
+use easy_cast::Cast;
+use itertools::Itertools;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-enum FishNumberToken {
-    Open,
-    Close,
-    Number(Number),
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum SnailfishNumber {
+    RegularNumber(RegularNumber),
+    Pair(Box<SnailfishNumber>, Box<SnailfishNumber>),
 }
 
-impl Display for FishNumberToken {
+impl Display for SnailfishNumber {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            FishNumberToken::Open => write!(f, "["),
-            FishNumberToken::Close => write!(f, "]"),
-            FishNumberToken::Number(number) => write!(f, "{number}"),
+            SnailfishNumber::RegularNumber(regular_number) => write!(f, "{regular_number}"),
+            SnailfishNumber::Pair(left, right) => {
+                write!(f, "[{left},{right}]")
+            }
         }
     }
 }
 
-type FishNumber = Vec<FishNumberToken>;
+type RegularNumber = usize;
+type Magnitude = usize;
 
 pub fn first(input: &str) -> String {
-    let sum = input
-        .lines()
-        .map(parse_fish_number)
-        .reduce(|sum, addend| add(&sum, &addend))
-        .expect("at least one fish number should be present");
-    magnitude(&sum).to_string()
+    let snailfish_numbers = snailfish_numbers(input);
+    magnitude(&sum(snailfish_numbers)).to_string()
 }
 
 pub fn second(input: &str) -> String {
-    let fish_numbers: Vec<FishNumber> = input.lines().map(parse_fish_number).collect();
-    largest_magnitude_of_two_term_sum(&fish_numbers).to_string()
+    let snailfish_numbers = snailfish_numbers(input);
+    magnitudes_all_pairs(snailfish_numbers)
+        .max()
+        .expect("at least one pair should exist")
+        .to_string()
 }
 
-fn parse_fish_number(fish_number: &str) -> FishNumber {
-    fish_number
-        .chars()
-        .filter(|&char| char != ',')
-        .map(|char| match char {
-            '[' => FishNumberToken::Open,
-            ']' => FishNumberToken::Close,
-            '0'..='9' => FishNumberToken::Number(
-                char.to_digit(10)
-                    .expect("only digits should be converted to numbers") as usize,
-            ),
-            _ => panic!("input should only contain brackets, commas, digits, and not {char:?}"),
-        })
-        .collect()
+fn sum(snailfish_numbers: impl Iterator<Item = SnailfishNumber>) -> SnailfishNumber {
+    snailfish_numbers
+        .reduce(add)
+        .expect("at least one snailfish number should be present")
 }
 
-type Index = usize;
-
-enum Reduction {
-    Explotion(Index),
-    Split(Index),
+fn magnitudes_all_pairs(
+    snailfish_numbers: impl Iterator<Item = SnailfishNumber>,
+) -> impl Iterator<Item = Magnitude> {
+    snailfish_numbers.permutations(2).map(|pair| {
+        let [left, right] = pair.try_into().expect("pair should contain two elements");
+        magnitude(&add(left, right))
+    })
 }
 
-fn reduction(fish_number: &[FishNumberToken]) -> Option<Reduction> {
-    let mut depth = 0;
-    let mut large_number_index = None;
-    for (index, &token) in fish_number.iter().enumerate() {
-        match token {
-            FishNumberToken::Open => {
-                if depth == 4 {
-                    return Some(Reduction::Explotion(index));
-                }
-                depth += 1;
-            }
-            FishNumberToken::Close => {
-                depth -= 1;
-            }
-            FishNumberToken::Number(number) => {
-                if large_number_index.is_none() && number >= 10 {
-                    large_number_index = Some(Reduction::Split(index));
-                }
+fn magnitude(snailfish_number: &SnailfishNumber) -> Magnitude {
+    match snailfish_number {
+        &SnailfishNumber::RegularNumber(regular_number) => regular_number,
+        SnailfishNumber::Pair(left, right) => 3 * magnitude(left) + 2 * magnitude(right),
+    }
+}
+
+fn add(left: SnailfishNumber, right: SnailfishNumber) -> SnailfishNumber {
+    reduced(SnailfishNumber::Pair(Box::new(left), Box::new(right)))
+}
+
+fn reduced(mut snailfish_number: SnailfishNumber) -> SnailfishNumber {
+    while reduce(&mut snailfish_number) {}
+    snailfish_number
+}
+
+fn reduce(snailfish_number: &mut SnailfishNumber) -> bool {
+    maybe_explode(snailfish_number, 0, None, None) || maybe_split(snailfish_number)
+}
+
+fn maybe_explode(
+    snailfish_number: &mut SnailfishNumber,
+    depth: usize,
+    nearest_predecessor: Option<&mut SnailfishNumber>,
+    nearest_successor: Option<&mut SnailfishNumber>,
+) -> bool {
+    match snailfish_number {
+        SnailfishNumber::RegularNumber(_) => false,
+        SnailfishNumber::Pair(left, right) => {
+            if depth >= 4 {
+                explode(snailfish_number, nearest_predecessor, nearest_successor);
+                true
+            } else {
+                maybe_explode(left, depth + 1, nearest_predecessor, Some(right))
+                    || maybe_explode(right, depth + 1, Some(left), nearest_successor)
             }
         }
     }
-    large_number_index
 }
 
-fn add_to_first_number<'tokens>(
-    tokens: impl IntoIterator<Item = &'tokens mut FishNumberToken>,
-    addend: Number,
+fn maybe_split(snailfish_number: &mut SnailfishNumber) -> bool {
+    match snailfish_number {
+        &mut SnailfishNumber::RegularNumber(regular_number) => {
+            let splits = regular_number >= 10;
+            if splits {
+                split(snailfish_number);
+            }
+            splits
+        }
+        SnailfishNumber::Pair(left, right) => maybe_split(left) || maybe_split(right),
+    }
+}
+
+fn explode(
+    snailfish_number: &mut SnailfishNumber,
+    nearest_predecessor: Option<&mut SnailfishNumber>,
+    nearest_successor: Option<&mut SnailfishNumber>,
 ) {
-    for token in tokens {
-        if let FishNumberToken::Number(number) = token {
-            *number += addend;
-            return;
-        }
-    }
-}
-
-fn explode(fish_number: &mut FishNumber, index: usize) {
-    let FishNumberToken::Number(left) = fish_number[index + 1] else {
-        panic!("first token after an open bracket should be the left number of a pair");
+    let SnailfishNumber::Pair(left, right) = snailfish_number else {
+        panic!("only pairs should explode");
     };
-    add_to_first_number(fish_number[..index].iter_mut().rev(), left);
-
-    let FishNumberToken::Number(right) = fish_number[index + 2] else {
-        panic!("second token after an open bracket should be the right number of a pair");
+    let (SnailfishNumber::RegularNumber(left), SnailfishNumber::RegularNumber(right)) =
+        (left.as_ref(), right.as_ref())
+    else {
+        panic!("members of exploding pair should be regular numbers");
     };
-    add_to_first_number(fish_number[index + 4..].iter_mut(), right);
 
-    fish_number.splice(index..index + 4, [FishNumberToken::Number(0)]);
+    if let Some(nearest_predecessor) = nearest_predecessor {
+        let SnailfishNumber::RegularNumber(left_neighbor) = rightmost_child(nearest_predecessor)
+        else {
+            panic!("left neighbor should be a regular number");
+        };
+        *left_neighbor += left;
+    }
+    if let Some(nearest_successor) = nearest_successor {
+        let SnailfishNumber::RegularNumber(right_neighbor) = leftmost_child(nearest_successor)
+        else {
+            panic!("right neighbor should be a regular number");
+        };
+        *right_neighbor += right;
+    }
+
+    *snailfish_number = SnailfishNumber::RegularNumber(0);
 }
 
-fn split(fish_number: &mut FishNumber, index: usize) {
-    let FishNumberToken::Number(number) = fish_number[index] else {
-        panic!("only numbers should be split");
+fn leftmost_child(mut snailfish_number: &mut SnailfishNumber) -> &mut SnailfishNumber {
+    while let SnailfishNumber::Pair(left, _) = snailfish_number {
+        snailfish_number = left.as_mut();
+    }
+    snailfish_number
+}
+
+fn rightmost_child(mut snailfish_number: &mut SnailfishNumber) -> &mut SnailfishNumber {
+    while let SnailfishNumber::Pair(_, right) = snailfish_number {
+        snailfish_number = right.as_mut();
+    }
+    snailfish_number
+}
+
+fn split(snailfish_number: &mut SnailfishNumber) {
+    let &mut SnailfishNumber::RegularNumber(regular_number) = snailfish_number else {
+        panic!("only regular numbers should split");
     };
-    let (left, right) = (number / 2, (number + 1) / 2);
-    debug_assert_eq!(left + right, number);
-    let pair = [
-        FishNumberToken::Open,
-        FishNumberToken::Number(left),
-        FishNumberToken::Number(right),
-        FishNumberToken::Close,
-    ];
-    fish_number.splice(index..index + 1, pair);
+    let [left, right] = [half_floor, half_ceil]
+        .map(|half| Box::new(SnailfishNumber::RegularNumber(half(regular_number))));
+    *snailfish_number = SnailfishNumber::Pair(left, right);
 }
 
-fn reduce(fish_number: &mut FishNumber) {
-    while let Some(reduction) = reduction(fish_number) {
-        match reduction {
-            Reduction::Explotion(index) => explode(fish_number, index),
-            Reduction::Split(index) => split(fish_number, index),
+fn half_floor(number: RegularNumber) -> RegularNumber {
+    number / 2
+}
+
+fn half_ceil(number: RegularNumber) -> RegularNumber {
+    let odd = number & 1;
+    number / 2 + odd
+}
+
+fn snailfish_numbers(input: &str) -> impl Iterator<Item = SnailfishNumber> + '_ {
+    input
+        .lines()
+        .map(|line| snailfish_number(line.as_bytes()).1)
+}
+
+fn snailfish_number(bytes: &[u8]) -> (&[u8], SnailfishNumber) {
+    match bytes[0] {
+        b'[' => {
+            let (remaining, left) = snailfish_number(&bytes[1..]);
+            debug_assert_eq!(remaining[0], b',', "pair should be separated by ','");
+            let (remaining, right) = snailfish_number(&remaining[1..]);
+            debug_assert_eq!(remaining[0], b']', "pair should end with ']'");
+            let pair = SnailfishNumber::Pair(Box::new(left), Box::new(right));
+            (&remaining[1..], pair)
         }
-    }
-}
-
-fn add(left: &[FishNumberToken], right: &[FishNumberToken]) -> FishNumber {
-    let mut sum = Vec::from([FishNumberToken::Open]);
-    sum.extend_from_slice(left);
-    sum.extend_from_slice(right);
-    sum.push(FishNumberToken::Close);
-    reduce(&mut sum);
-    sum
-}
-
-fn magnitude(tokens: &[FishNumberToken]) -> Number {
-    fn magnitude(tokens: &[FishNumberToken]) -> (&[FishNumberToken], Number) {
-        match tokens[0] {
-            FishNumberToken::Open => {
-                let (remaining, left) = magnitude(&tokens[1..]);
-                let (remaining, right) = magnitude(remaining);
-                (&remaining[1..], 3 * left + 2 * right)
-            }
-            FishNumberToken::Number(number) => (&tokens[1..], number),
-            FishNumberToken::Close => {
-                panic!("close bracket should not be the first token of a fish number")
-            }
+        b'0'..=b'9' => {
+            let regular_number = (bytes[0] - b'0').cast();
+            (&bytes[1..], SnailfishNumber::RegularNumber(regular_number))
         }
+        _ => panic!("first character in pair should be '[' or digit"),
     }
-
-    let (remaining, magnitude) = magnitude(tokens);
-    debug_assert!(remaining.is_empty());
-    magnitude
-}
-
-fn largest_magnitude_of_two_term_sum(fish_numbers: &[FishNumber]) -> Number {
-    let mut largest_magnitude = Number::MIN;
-    for left_index in 0..fish_numbers.len() {
-        for right_index in 0..fish_numbers.len() {
-            if left_index == right_index {
-                continue;
-            }
-            largest_magnitude = max(
-                largest_magnitude,
-                magnitude(&add(&fish_numbers[left_index], &fish_numbers[right_index])),
-            );
-        }
-    }
-    largest_magnitude
 }
 
 #[cfg(test)]
 mod tests {
-    use infrastructure::{Input, Puzzle};
+    use infrastructure::{test, Input, Puzzle};
 
     use super::*;
     use crate::tests::{input, test_on_input};
@@ -207,26 +222,30 @@ mod tests {
     }
 
     #[test]
-    fn parse() {
-        let numbers = input(DAY, Input::PuzzleInput);
-        for number in numbers.lines() {
-            assert_eq!(display_fish_number(&parse_fish_number(number)), number);
-        }
+    fn sum() {
+        let function = |example| {
+            let input = input(DAY, Input::Example(example));
+            super::sum(snailfish_numbers(&input)).to_string()
+        };
+        let cases = [
+            (1, "[[[[1,1],[2,2]],[3,3]],[4,4]]".to_owned()),
+            (2, "[[[[3,0],[5,3]],[4,4]],[5,5]]".to_owned()),
+            (3, "[[[[5,0],[7,4]],[5,5]],[6,6]]".to_owned()),
+            (
+                4,
+                "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]".to_owned(),
+            ),
+        ];
+        test::cases(function, cases);
     }
 
-    fn display_fish_number(fish_number: &[FishNumberToken]) -> String {
-        let mut result = String::new();
-        for index in 0..fish_number.len() {
-            let current_token = fish_number[index];
-            result.push_str(&current_token.to_string());
-            if current_token != FishNumberToken::Open {
-                if let Some(&token) = fish_number.get(index + 1) {
-                    if token != FishNumberToken::Close {
-                        result.push(',');
-                    }
-                }
-            }
-        }
-        result
+    #[test]
+    fn reduced() {
+        let (remaining, snailfish_number) =
+            snailfish_number(b"[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]");
+        assert!(remaining.is_empty(), "entire input should be parsed");
+        let actual = super::reduced(snailfish_number);
+        let expected = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]";
+        assert_eq!(actual.to_string(), expected);
     }
 }
