d96233779b1897aa18f9655c40a0438cf9592173
Patrick Kasting <patrick.kasting@gmail.com>
2024-11-20 20:00:39 +0100

    Parsing

--- advent20/src/day24.rs
+++ advent20/src/day24.rs
@@ -1,9 +1,47 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    East,
+    SouthEast,
+    SouthWest,
+    West,
+    NorthWest,
+    NorthEast,
+}
+
 pub fn first_answer(input: &str) -> String {
     todo!()
 }
 
 pub fn second_answer(input: &str) -> String {
     todo!()
+}
+
+fn walks(input: &str) -> impl Iterator<Item = Vec<Direction>> + '_ {
+    input.lines().map(walk)
+}
+
+fn walk(mut str: &str) -> Vec<Direction> {
+    let mut walk = vec![];
+    while !str.is_empty() {
+        let (remaining, direction) = direction(str);
+        str = remaining;
+        walk.push(direction);
+    }
+    walk
+}
+
+fn direction(str: &str) -> (&str, Direction) {
+    match &str[0..1] {
+        "e" => (&str[1..], Direction::East),
+        "w" => (&str[1..], Direction::West),
+        _ => match &str[0..2] {
+            "se" => (&str[2..], Direction::SouthEast),
+            "sw" => (&str[2..], Direction::SouthWest),
+            "nw" => (&str[2..], Direction::NorthWest),
+            "ne" => (&str[2..], Direction::NorthEast),
+            _ => panic!("direction should be 'e', 'se', 'sw', 'w', 'nw', or 'ne'"),
+        },
+    }
 }
 
 #[cfg(test)]
