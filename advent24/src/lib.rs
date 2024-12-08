a4f68881e02bcb2f2eccb3f65793252225ba6e48
Patrick Kasting <patrick.kasting@gmail.com>
2024-12-08 17:31:46 +0100

    Solved day 7 of 2024

--- advent24/src/lib.rs
+++ advent24/src/lib.rs
@@ -3,6 +3,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day07;
 
 const SOLUTIONS: &[[infrastructure::Solution; 2]] = &[
     [day01::first_answer, day01::second_answer],
@@ -10,6 +11,8 @@ const SOLUTIONS: &[[infrastructure::Solution; 2]] = &[
     [day03::first_answer, day03::second_answer],
     [day04::first_answer, day04::second_answer],
     [day05::first_answer, day05::second_answer],
+    [|_| unimplemented!(), |_| unimplemented!()],
+    [day07::first_answer, day07::second_answer],
 ];
 
 /// # Errors
