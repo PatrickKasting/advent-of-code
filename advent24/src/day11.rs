b99d60c1ab28893a0d0bbc52c55509f6035fbc01
Patrick Kasting <patrick.kasting@gmail.com>
2026-02-17 19:13:17 +0100

    Solved part two

--- advent24/src/day11.rs
+++ advent24/src/day11.rs
@@ -1,41 +1,63 @@
-use itertools::Itertools;
+use ahash::AHashMap;
 use shared::{number_theory::number_of_decimal_digits, string::isizes};
 
 type Line = Vec<Stone>;
 type Stone = isize;
 
 pub fn first_answer(input: &str) -> String {
-    let mut line = isizes(input);
-    for _ in 0..25 {
-        line = next_line(line);
-    }
-    line.len().to_string()
+    final_number_of_stones(line(input), 25).to_string()
 }
 
 pub fn second_answer(input: &str) -> String {
-    todo!()
+    final_number_of_stones(line(input), 75).to_string()
 }
 
-fn next_line(line: Line) -> Line {
-    line.into_iter().flat_map(next_stone).collect_vec()
+fn final_number_of_stones(line: Line, number_of_steps: usize) -> usize {
+    let mut cache = AHashMap::new();
+    line.into_iter()
+        .map(|stone| number_of_stones(&mut cache, stone, number_of_steps))
+        .sum::<usize>()
 }
 
-fn next_stone(stone: Stone) -> Vec<Stone> {
+fn number_of_stones(
+    cache: &mut AHashMap<(Stone, usize), usize>,
+    stone: Stone,
+    number_of_steps: usize,
+) -> usize {
+    if number_of_steps == 0 {
+        return 1;
+    }
+    if let Some(&cached) = cache.get(&(stone, number_of_steps)) {
+        return cached;
+    }
+
     let number_of_digits = number_of_decimal_digits(stone);
-    if stone == 0 {
-        vec![1]
+    let number_of_stones = if stone == 0 {
+        number_of_stones(cache, 1, number_of_steps - 1)
     } else if number_of_digits % 2 == 0 {
         let half_number_of_digits = number_of_digits / 2;
         let divisor = 10_isize.pow(half_number_of_digits);
-        vec![stone / divisor, stone % divisor]
+        let mut stones = [stone / divisor, stone % divisor];
+        stones.sort_unstable();
+        let small = number_of_stones(cache, stones[0], number_of_steps - 1);
+        let large = number_of_stones(cache, stones[1], number_of_steps - 1);
+        small + large
     } else {
-        vec![stone * 2024]
-    }
+        number_of_stones(cache, stone * 2024, number_of_steps - 1)
+    };
+    cache.insert((stone, number_of_steps), number_of_stones);
+    number_of_stones
+}
+
+fn line(input: &str) -> Line {
+    let mut line = isizes(input);
+    line.sort_unstable();
+    line
 }
 
 #[cfg(test)]
 mod tests {
-    use infrastructure::{test, Input, Puzzle};
+    use infrastructure::{Input, Puzzle};
 
     use crate::tests::test_on_input;
 
@@ -48,22 +70,16 @@ mod tests {
 
     #[test]
     fn first_answer_input() {
-        test_on_input(DAY, Puzzle::First, Input::PuzzleInput, 216042);
-    }
-
-    #[test]
-    fn second_answer_example() {
-        test_on_input(DAY, Puzzle::Second, Input::Example(0), 34);
+        test_on_input(DAY, Puzzle::First, Input::PuzzleInput, 216_042);
     }
 
     #[test]
     fn second_answer_input() {
-        test_on_input(DAY, Puzzle::Second, Input::PuzzleInput, 1200);
-    }
-
-    #[test]
-    fn next_stone() {
-        let cases = [(0, vec![1]), (2973, vec![29, 73]), (973, vec![1_969_352])];
-        test::cases(super::next_stone, cases);
+        test_on_input(
+            DAY,
+            Puzzle::Second,
+            Input::PuzzleInput,
+            255_758_646_442_399_usize,
+        );
     }
 }
