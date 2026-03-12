4e0e98720a0d82d285fc5bde516e159c6d0afe01
Patrick Kasting <patrick.kasting@gmail.com>
2026-03-12 13:14:07 +0100

    Small refactor

--- advent24/src/day14.rs
+++ advent24/src/day14.rs
@@ -1,8 +1,10 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, fs};
 
 use easy_cast::Cast;
+use image::{imageops::FilterType, ImageBuffer};
 use itertools::Itertools;
 use shared::{string::isizes, vector::Vector};
+use tflitec::{interpreter::Interpreter, model::Model};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Robot {
@@ -13,7 +15,16 @@ type Position = [Coordinate; 2];
 type Velocity = [Coordinate; 2];
 type Coordinate = isize;
 
-const DIMENSIONS: [Coordinate; 2] = [101, 103];
+const SPACE_DIMENSIONS: [Coordinate; 2] = [101, 103];
+
+pub fn first_answer(input: &str) -> String {
+    safety_factor(robots(input), 100).to_string()
+}
+
+pub fn second_answer(input: &str) -> String {
+    let robots = robots(input).collect_vec();
+    christmas_tree_time(&robots).to_string()
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Quadrant {
@@ -22,16 +33,8 @@ enum Quadrant {
     BottomLeft,
     TopLeft,
 }
-const BOUNDARIES: [Coordinate; 2] = [DIMENSIONS[0] / 2, DIMENSIONS[1] / 2];
 
-pub fn first_answer(input: &str) -> String {
-    safety_factor(robots(input), 100).to_string()
-}
-
-pub fn second_answer(input: &str) -> String {
-    let robots = robots(input).collect_vec();
-    christmas_tree(&robots).to_string()
-}
+const QUADRANT_BORDERS: [Coordinate; 2] = [SPACE_DIMENSIONS[0] / 2, SPACE_DIMENSIONS[1] / 2];
 
 fn safety_factor(robots: impl Iterator<Item = Robot>, seconds: usize) -> usize {
     let counts = robots.filter_map(|robot| quadrant(robot, seconds)).counts();
@@ -40,7 +43,7 @@ fn safety_factor(robots: impl Iterator<Item = Robot>, seconds: usize) -> usize {
 
 fn quadrant(robot: Robot, seconds: usize) -> Option<Quadrant> {
     let [x, y] = position(robot, seconds);
-    match [x.cmp(&BOUNDARIES[0]), y.cmp(&BOUNDARIES[1])] {
+    match [x.cmp(&QUADRANT_BORDERS[0]), y.cmp(&QUADRANT_BORDERS[1])] {
         [Ordering::Less, Ordering::Less] => Some(Quadrant::TopLeft),
         [Ordering::Less, Ordering::Greater] => Some(Quadrant::BottomLeft),
         [Ordering::Greater, Ordering::Less] => Some(Quadrant::TopRight),
@@ -49,61 +52,65 @@ fn quadrant(robot: Robot, seconds: usize) -> Option<Quadrant> {
     }
 }
 
-const INPUT_DIMENSION: usize = 28;
+const MODEL_INPUT_DIMENSION: usize = 28;
 
-fn christmas_tree(robots: &[Robot]) -> usize {
+fn christmas_tree_time(robots: &[Robot]) -> usize {
     let machine_learning_directory = [env!("CARGO_MANIFEST_DIR"), "/machine-learning/14"].join("");
 
     let model_path = [&machine_learning_directory, "/quickdraw_model_int8.tflite"].join("");
-    let model = tflitec::model::Model::new(&model_path).expect("model should load");
-    let interpreter = tflitec::interpreter::Interpreter::new(&model, None)
-        .expect("tensorflow lite should not fail");
+    let model = Model::new(&model_path).expect("model should load");
+    let interpreter = Interpreter::new(&model, None).expect("tensorflow lite should not fail");
     interpreter
         .allocate_tensors()
         .expect("tensors should allocate");
 
     let labels_path = [&machine_learning_directory, "/labels.txt"].join("");
-    let labels = std::fs::read_to_string(labels_path).expect("labels should be read");
+    let labels = fs::read_to_string(labels_path).expect("labels should be read");
     let fireplace = labels
         .lines()
         .position(|label| label == "fireplace")
         .expect("fireplace should be a label");
 
-    for second in 0.. {
-        let image = scaled_image(robots, second);
-        interpreter
-            .copy(&image, 0)
-            .expect("image data should copy to tensor");
-        interpreter.invoke().expect("model invocation should work");
-        let output_tensor = interpreter
-            .output(0)
-            .expect("output tensor should be accessable");
-        let output_data: &[u8] = output_tensor.data();
-
-        let cassification = output_data
-            .iter()
-            .position_max()
-            .expect("output data should be 345 values");
-        if cassification == fireplace {
-            return second;
+    for seconds in 0.. {
+        let image = scaled_image(robots, seconds);
+        if classification(&interpreter, &image) == fireplace {
+            return seconds;
         }
     }
     unreachable!("robots should resemble a christmas tree");
 }
 
-fn scaled_image(robots: &[Robot], seconds: usize) -> image::ImageBuffer<image::Luma<u8>, Vec<u8>> {
+fn classification(
+    interpreter: &Interpreter<'_>,
+    image: &ImageBuffer<image::Luma<u8>, Vec<u8>>,
+) -> usize {
+    interpreter
+        .copy(image, 0)
+        .expect("image data should copy to tensor");
+    interpreter.invoke().expect("model invocation should work");
+    let output_tensor = interpreter
+        .output(0)
+        .expect("output tensor should be accessable");
+    let output_data: &[u8] = output_tensor.data();
+    output_data
+        .iter()
+        .position_max()
+        .expect("output data should be 345 values")
+}
+
+fn scaled_image(robots: &[Robot], seconds: usize) -> ImageBuffer<image::Luma<u8>, Vec<u8>> {
     image::imageops::resize(
         &image(robots, seconds),
-        INPUT_DIMENSION.cast(),
-        INPUT_DIMENSION.cast(),
-        image::imageops::FilterType::Lanczos3,
+        MODEL_INPUT_DIMENSION.cast(),
+        MODEL_INPUT_DIMENSION.cast(),
+        FilterType::Lanczos3,
     )
 }
 
-fn image(robots: &[Robot], seconds: usize) -> image::ImageBuffer<image::Luma<u8>, Vec<u8>> {
-    let mut image = image::ImageBuffer::from_pixel(
-        DIMENSIONS[0].cast(),
-        DIMENSIONS[1].cast(),
+fn image(robots: &[Robot], seconds: usize) -> ImageBuffer<image::Luma<u8>, Vec<u8>> {
+    let mut image = ImageBuffer::from_pixel(
+        SPACE_DIMENSIONS[0].cast(),
+        SPACE_DIMENSIONS[1].cast(),
         image::Luma([u8::MAX]),
     );
     for [x, y] in robots.iter().map(|&robot| position(robot, seconds)) {
@@ -114,7 +121,10 @@ fn image(robots: &[Robot], seconds: usize) -> image::ImageBuffer<image::Luma<u8>
 
 fn position(Robot { position, velocity }: Robot, seconds: usize) -> Position {
     let [x, y] = position.add(velocity.mul(seconds.cast()));
-    [x.rem_euclid(DIMENSIONS[0]), y.rem_euclid(DIMENSIONS[1])]
+    [
+        x.rem_euclid(SPACE_DIMENSIONS[0]),
+        y.rem_euclid(SPACE_DIMENSIONS[1]),
+    ]
 }
 
 fn robots(input: &str) -> impl Iterator<Item = Robot> + use<'_> {
@@ -143,6 +153,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "this takes several minutes"]
     fn second_answer_input() {
         test_on_input(DAY, Puzzle::Second, Input::PuzzleInput, 6587);
     }
