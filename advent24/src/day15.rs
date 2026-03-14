8cc799a04f21849869c3c4e2aacbc2361cbe9a21
Patrick Kasting <patrick.kasting@gmail.com>
2026-03-14 16:03:00 +0100

    Solved part one

--- advent24/src/day15.rs
+++ advent24/src/day15.rs
@@ -1,27 +1,71 @@
 use easy_cast::Conv;
-use shared::grid::{self, Direction, Grid, Position};
+use shared::{
+    grid::{self, Direction, Grid, Position},
+    vector::Vector,
+};
 
 type Warehouse = Grid<u8>;
+type GpsCoordinate = usize;
 
 pub fn first_answer(input: &str) -> String {
-    todo!()
+    let (mut warehouse, directions) = warehouse_and_directions(input);
+    sum_of_gps_coordinates(&mut warehouse, directions).to_string()
 }
 
 pub fn second_answer(input: &str) -> String {
     todo!()
 }
 
-fn gps_coordinate([row, column]: Position) -> usize {
+fn sum_of_gps_coordinates(
+    warehouse: &mut Warehouse,
+    directions: impl Iterator<Item = Direction>,
+) -> GpsCoordinate {
+    state_after(warehouse, directions);
+    warehouse
+        .iter_row_major()
+        .filter(|&(_, &element)| element == b'O')
+        .map(|(position, _)| gps_coordinate(position))
+        .sum()
+}
+
+fn state_after(warehouse: &mut Warehouse, directions: impl Iterator<Item = Direction>) {
+    for direction in directions {
+        movement(warehouse, direction);
+    }
+}
+
+fn movement(warehouse: &mut Warehouse, direction: Direction) {
+    let (robot, _) = warehouse
+        .find(|_, &element| element == b'@')
+        .expect("robot should be present");
+    let mut current = robot;
+    loop {
+        current = current.add(direction);
+        match warehouse[current] {
+            b'O' => (),
+            b'#' => return,
+            b'.' => {
+                warehouse[current] = b'O';
+                warehouse[robot.add(direction)] = b'@';
+                warehouse[robot] = b'.';
+                return;
+            }
+            _ => panic!("warehouse should contain only expected elements"),
+        }
+    }
+}
+
+fn gps_coordinate([row, column]: Position) -> GpsCoordinate {
     100 * usize::conv(row) + usize::conv(column)
 }
 
-fn warehouse_and_movements(input: &str) -> (Warehouse, impl Iterator<Item = Direction> + use<'_>) {
-    let (warehouse, movements) = input
+fn warehouse_and_directions(input: &str) -> (Warehouse, impl Iterator<Item = Direction> + use<'_>) {
+    let (warehouse, directions) = input
         .split_once("\n\n")
-        .expect("warehouse and movements should be separated by an empty line");
+        .expect("warehouse and directions should be separated by an empty line");
     let warehouse = Warehouse::from(warehouse);
-    let movements = movements.chars().filter_map(grid::direction).cycle();
-    (warehouse, movements)
+    let directions = directions.chars().filter_map(grid::direction);
+    (warehouse, directions)
 }
 
 #[cfg(test)]
@@ -38,10 +82,10 @@ mod tests {
         test_on_input(DAY, Puzzle::First, Input::Example(1), 2028);
     }
 
-    // #[test]
-    // fn first_answer_input() {
-    //     test_on_input(DAY, Puzzle::First, Input::PuzzleInput, 396);
-    // }
+    #[test]
+    fn first_answer_input() {
+        test_on_input(DAY, Puzzle::First, Input::PuzzleInput, 396);
+    }
 
     // #[test]
     // fn second_answer_example() {
