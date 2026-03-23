6482d0c294874c06c3f56513996560568e108e74
Patrick Kasting <patrick.kasting@gmail.com>
2026-03-23 17:57:53 +0100

    TEMPORARY COMMIT!

--- advent24/src/day15.rs
+++ advent24/src/day15.rs
@@ -10,7 +10,7 @@ type GpsCoordinate = usize;
 
 pub fn first_answer(input: &str) -> String {
     let (mut warehouse, directions) = warehouse_and_directions(input);
-    movements(&mut warehouse, directions, movement);
+    moves(&mut warehouse, directions);
     gps_coordinates(&warehouse)
         .sum::<GpsCoordinate>()
         .to_string()
@@ -19,24 +19,22 @@ pub fn first_answer(input: &str) -> String {
 pub fn second_answer(input: &str) -> String {
     let (warehouse, directions) = warehouse_and_directions(input);
     let mut wide_warehouse = wide_warehouse(&warehouse);
-    println!("{wide_warehouse}");
-    movements(&mut wide_warehouse, directions, wide_robot_movement);
+    print!("{wide_warehouse}");
+    moves(&mut wide_warehouse, directions);
     gps_coordinates(&wide_warehouse)
         .sum::<GpsCoordinate>()
         .to_string()
 }
 
-fn movements(
-    warehouse: &mut Warehouse,
-    directions: impl Iterator<Item = Direction>,
-    mut movement: impl FnMut(&mut Warehouse, Position, Direction) -> bool,
-) {
+fn moves(warehouse: &mut Warehouse, directions: impl Iterator<Item = Direction>) {
     let mut robot = robot_position(warehouse);
     for direction in directions {
-        if movement(warehouse, robot, direction) {
+        let width = warehouse.width();
+        println!("{:^width$}", char_from(direction));
+        if r#move(warehouse, robot, direction) {
             robot = robot.add(direction);
         }
-        println!("{warehouse}");
+        print!("{warehouse}");
     }
 }
 
@@ -47,114 +45,79 @@ fn robot_position(warehouse: &mut Grid<u8>) -> Position {
         .0
 }
 
-fn movement(warehouse: &mut Warehouse, from: Position, direction: Direction) -> bool {
+fn r#move(warehouse: &mut Warehouse, from: Position, direction: Direction) -> bool {
     let to = from.add(direction);
     let element = warehouse[to];
-    match element {
+    let is_possible = match element {
         b'#' => false,
-        b'O' if !movement(warehouse, to, direction) => false,
-        b'.' | b'O' => {
-            [warehouse[from], warehouse[to]] = [b'.', warehouse[from]];
-            true
-        }
+        b'O' => r#move(warehouse, to, direction),
+        b'[' => move_wide_box(warehouse, to, direction),
+        b']' => move_wide_box(warehouse, to.add(WEST), direction),
+        b'.' => true,
         _ => panic!("warehouse should contain only expected elements"),
+    };
+    if is_possible {
+        [warehouse[from], warehouse[to]] = [b'.', warehouse[from]];
     }
+    is_possible
 }
 
-fn wide_warehouse(warehouse: &Warehouse) -> Warehouse {
-    let elements = warehouse
-        .iter_row_major()
-        .flat_map(|(_, &element)| match element {
-            b'#' => b"##",
-            b'O' => b"[]",
-            b'.' => b"..",
-            b'@' => b"@.",
-            _ => panic!("warehouse should contain only expected elements"),
-        })
-        .copied()
-        .collect_vec();
-    Warehouse::from_elements(elements, warehouse.width() * 2)
+fn move_wide_box(warehouse: &mut Warehouse, west: Position, direction: Direction) -> bool {
+    let Some(boxes) = (match direction {
+        EAST | WEST => wide_boxes_east_west(warehouse, west, direction),
+        NORTH | SOUTH => wide_boxes_north_south(warehouse, west, direction),
+        _ => panic!("direction should be orthogonal"),
+    }) else {
+        return false;
+    };
+    for west in boxes {
+        let to_west = west.add(direction);
+        let [east, to_east] = [west, to_west].map(|p| p.add(EAST));
+        [warehouse[west], warehouse[east]] = [b'.', b'.'];
+        [warehouse[to_west], warehouse[to_east]] = [b'[', b']'];
+    }
+    true
 }
 
-fn wide_robot_movement(warehouse: &mut Warehouse, from: Position, direction: Direction) -> bool {
-    let to = from.add(direction);
-    let element = warehouse[to];
+fn wide_boxes_east_west(
+    warehouse: &mut Grid<u8>,
+    west: Position,
+    direction: Direction,
+) -> Option<Vec<Position>> {
+    let append = |vec| [vec, vec![west]].concat();
+    let to_west = west.add(direction);
+    let next = to_west.add(direction);
+    let neighbor = to_west.add(direction.mul(isize::from(direction == EAST)));
+    let element = warehouse[neighbor];
     match element {
-        b'#' => false,
-        b'[' if !wide_box_movement(warehouse, to, direction) => false,
-        b']' if !wide_box_movement(warehouse, to.add(WEST), direction) => false,
-        b'.' | b'[' | b']' => {
-            [warehouse[from], warehouse[to]] = [b'.', b'@'];
-            true
-        }
-        _ => panic!("warehouse should contain only expected elements"),
+        b'#' => None,
+        b'[' | b']' => wide_boxes_east_west(warehouse, next, direction).map(append),
+        b'.' => Some(vec![west]),
+        _ => panic!("wide warehouse should contain only expected elements"),
     }
 }
 
-fn wide_box_movement(warehouse: &mut Warehouse, west: Position, direction: Direction) -> bool {
-    if [EAST, WEST].contains(&direction) {
-        let to = west.add(direction);
-        let next = to.add(direction);
-        let element = warehouse[next];
-        match element {
-            b'#' => false,
-            b'[' if !wide_box_movement(warehouse, next, direction) => false,
-            b'.' | b'[' => {
-                if direction == EAST {
-                    [warehouse[west], warehouse[to], warehouse[next]] = [b'.', b'[', b']'];
-                } else {
-                    let east = west.add(EAST);
-                    [warehouse[to], warehouse[west], warehouse[east]] = [b'[', b']', b'.'];
-                }
-                true
-            }
-            _ => panic!("warehouse should contain only expected elements"),
-        }
-    } else if [NORTH, SOUTH].contains(&direction) {
-        if can_vertically_move_boxes(warehouse, west, direction) {
-            vertically_move_boxes(warehouse, west, direction);
-            true
-        } else {
-            false
-        }
-    } else {
-        panic!("direction should be north, east, south, or west");
-    }
-}
-
-fn can_vertically_move_boxes(warehouse: &Warehouse, west: Position, direction: Direction) -> bool {
+fn wide_boxes_north_south(
+    warehouse: &mut Grid<u8>,
+    west: Position,
+    direction: Direction,
+) -> Option<Vec<Position>> {
+    let append = |vec| [vec, vec![west]].concat();
     let east = west.add(EAST);
-    let [to_west, to_east] = [west, east].map(|position| position.add(direction));
-    let [west_element, east_element] = [to_west, to_east].map(|position| warehouse[position]);
+    let [to_west, to_east] = [west, east].map(|p| p.add(direction));
+    let [west_element, east_element] = [to_west, to_east].map(|p| warehouse[p]);
     match [west_element, east_element] {
-        [b'#', _] | [_, b'#'] => false,
-        [b'[', b']'] => can_vertically_move_boxes(warehouse, to_west, direction),
-        [b']', _] if !can_vertically_move_boxes(warehouse, to_west.add(WEST), direction) => false,
-        [_, b'['] if !can_vertically_move_boxes(warehouse, to_east, direction) => false,
-        _ => true,
+        [b'#', _] | [_, b'#'] => None,
+        [b'[', b']'] => wide_boxes_north_south(warehouse, to_west, direction).map(append),
+        [b']', b'['] => {
+            wide_boxes_north_south(warehouse, to_west.add(WEST), direction).and_then(|lhs| {
+                wide_boxes_north_south(warehouse, to_east, direction).map(|rhs| [lhs, rhs].concat())
+            })
+        }
+        [b']', _] => wide_boxes_north_south(warehouse, to_west.add(WEST), direction).map(append),
+        [_, b'['] => wide_boxes_north_south(warehouse, to_east, direction).map(append),
+        _ => Some(vec![west]),
     }
-}
-
-fn vertically_move_boxes(warehouse: &mut Warehouse, west: Position, direction: Direction) {
-    let east = west.add(EAST);
-    let [to_west, to_east] = [west, east].map(|position| position.add(direction));
-    let [west_element, east_element] = [to_west, to_east].map(|position| warehouse[position]);
-    if west_element == b']' {
-        vertically_move_boxes(warehouse, to_west.add(WEST), direction);
-    }
-    if west_element == b'[' {
-        vertically_move_boxes(warehouse, to_west, direction);
-    }
-    if east_element == b'[' {
-        vertically_move_boxes(warehouse, to_east, direction);
-    }
-
-    [
-        warehouse[west],
-        warehouse[east],
-        warehouse[to_west],
-        warehouse[to_east],
-    ] = [b'.', b'.', b'[', b']'];
 }
 
 fn gps_coordinates(warehouse: &Warehouse) -> impl Iterator<Item = GpsCoordinate> + use<'_> {
@@ -175,6 +138,31 @@ fn warehouse_and_directions(input: &str) -> (Warehouse, impl Iterator<Item = Dir
     let warehouse = Warehouse::from(warehouse);
     let directions = directions.chars().filter_map(grid::direction);
     (warehouse, directions)
+}
+
+fn wide_warehouse(warehouse: &Warehouse) -> Warehouse {
+    let elements = warehouse
+        .iter_row_major()
+        .flat_map(|(_, &element)| match element {
+            b'#' => b"##",
+            b'O' => b"[]",
+            b'.' => b"..",
+            b'@' => b"@.",
+            _ => panic!("warehouse should contain only expected elements"),
+        })
+        .copied()
+        .collect_vec();
+    Warehouse::from_elements(elements, warehouse.width() * 2)
+}
+
+pub fn char_from(direction: Direction) -> char {
+    match direction {
+        NORTH => '^',
+        EAST => '>',
+        SOUTH => 'v',
+        WEST => '<',
+        _ => panic!("direction should be orthogonal"),
+    }
 }
 
 #[cfg(test)]
