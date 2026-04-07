943cffac7a1d61fc15b7068f27eb8bc59f739b40
Patrick Kasting <patrick.kasting@gmail.com>
2026-04-07 16:42:53 +0200

    Solved part two

--- advent24/src/day19.rs
+++ advent24/src/day19.rs
@@ -8,29 +8,38 @@ pub fn first_answer(input: &str) -> String {
     let (towels, designs) = towels_and_designs(input);
     let mut cache = AHashMap::new();
     designs
-        .filter(|design| is_possible(&mut cache, &towels, design))
+        .filter(|design| number_of_different_ways(&mut cache, &towels, design) > 0)
         .count()
         .to_string()
 }
 
 pub fn second_answer(input: &str) -> String {
-    todo!()
+    let (towels, designs) = towels_and_designs(input);
+    let mut cache = AHashMap::new();
+    designs
+        .map(|design| number_of_different_ways(&mut cache, &towels, design))
+        .sum::<usize>()
+        .to_string()
 }
 
-fn is_possible<'design>(
-    cache: &mut AHashMap<Design<'design>, bool>,
+fn number_of_different_ways<'design>(
+    cache: &mut AHashMap<Design<'design>, usize>,
     towels: &[Towel],
     design: Design<'design>,
-) -> bool {
+) -> usize {
     if let Some(&cached) = cache.get(design) {
         return cached;
     }
-    let result = design.is_empty()
-        || towels.iter().any(|towel| {
-            design
-                .strip_prefix(towel)
-                .is_some_and(|rest| is_possible(cache, towels, rest))
-        });
+    let result = match design {
+        "" => 1,
+        _ => towels
+            .iter()
+            .map(|towel| match design.strip_prefix(towel) {
+                Some(rest) => number_of_different_ways(cache, towels, rest),
+                None => 0,
+            })
+            .sum(),
+    };
     cache.insert(design, result);
     result
 }
@@ -62,13 +71,18 @@ mod tests {
         test_on_input(DAY, Puzzle::First, Input::PuzzleInput, 369);
     }
 
-    // #[test]
-    // fn second_answer_example() {
-    //     test_on_input(DAY, Puzzle::Second, Input::Example(0), 34);
-    // }
+    #[test]
+    fn second_answer_example() {
+        test_on_input(DAY, Puzzle::Second, Input::Example(0), 16);
+    }
 
-    // #[test]
-    // fn second_answer_input() {
-    //     test_on_input(DAY, Puzzle::Second, Input::PuzzleInput, 1200);
-    // }
+    #[test]
+    fn second_answer_input() {
+        test_on_input(
+            DAY,
+            Puzzle::Second,
+            Input::PuzzleInput,
+            761_826_581_538_190_usize,
+        );
+    }
 }
